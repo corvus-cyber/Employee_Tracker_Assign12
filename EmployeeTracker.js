b8da60ae3feebb9fb55802912e70d95863d3d103
Corvus <wyatta.hancock@gmail.com>
2020-08-10 10:22:24 -0600

    built main menu function

--- EmployeeTracker.js
+++ EmployeeTracker.js
@@ -20,5 +20,35 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   // run the start function after the connection is made to prompt the user
-  start();
+  menu();
 });
+
+//Start menu function, this will allow the user to choose which function they wish to be directed to
+function menu(){
+    inquirer.prompt(
+        {
+          type: "list",
+          name: "startMenu",
+          message: "Welcome to the Employee Tracker. What would you like to do today?",
+          choices: ["View Departments, roles and employees", "Add departments, roles, and employees", "Update Employee Roles", "Exit"]  
+        },
+        )
+        .then( response => {
+            switch(response.startMenu) {
+            case "View Departments, roles and employees":
+                view();
+                break;
+            case "Add departments, roles, and employees":
+                add();
+                break;
+            case "Update Employee Roles":
+                update();
+                break;
+            case "Exit":
+                console.log("Enjoy the rest of your day");
+                connection.end();
+                return;
+            }
+        });
+        
+};
