1492defbc2187fe78da964e2220e987d67893cd4
Corvus <wyatta.hancock@gmail.com>
2020-08-13 13:23:50 -0600

    delete role now works

--- db/EmployeeTracker.sql
+++ db/EmployeeTracker.sql
@@ -22,9 +22,9 @@ CREATE TABLE employee (
     id INTEGER(10) AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER(10) NOT NULL,
+    role_id INTEGER(10),
     manager_id INTEGER(10),
     PRIMARY KEY(id),
-    FOREIGN KEY(role_id) REFERENCES roles(id),
+    FOREIGN KEY(role_id) REFERENCES roles(id) ON DELETE SET NULL,
     FOREIGN KEY(manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
